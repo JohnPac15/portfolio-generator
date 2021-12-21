62b6bf3d88211deb72e33623b97f6ede21a5e484
JohnPac <johnpac15@yahoo.com>
2021-12-21 00:38:52 -0600

    added inquirer to the js file

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-const fs = require('fs')
+const inquirer = require('inquirer');
+// const fs = require('fs')
 
-const generatePage = require('./src/page-template.js')
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHtml = generatePage(Name, github)
 
-const [Name, github] = profileDataArgs
+// fs.writeFile('./index.html' , pageHtml, err => {
+//     if(err) throw (err);
 
-fs.writeFile('./index.html' , generatePage(Name, github), err => {
-    if(err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
-
-
-// const printProfileData = profileDataArr => {
-//     for(let i = 0; i < profileDataArr.length; i+= 1) {
-//         console.log(profileDataArr[i])
-//     }
-
-//     console.log('================')
-
-//     profileDataArr.forEach((profileItem) => console.log(profileItem))
-// };
-
-// printProfileData(profileDataArgs)
-
-// console.log(process.argv.slice(2))
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
